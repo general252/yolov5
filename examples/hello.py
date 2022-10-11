db6c47915c4fecf9ea5dfb72ecec4e5e43ea0f63
lichaunlin <lichaunlin@gmail.com>
2022-10-11 16:54:36 +0800

    use opencv2

--- examples/hello.py
+++ examples/hello.py
@@ -1,39 +1,38 @@
 import torch
 import os
-import warnings
-import models.common
+import cv2
 
 
-def f2():
+def hell1():
     print('---------------------load model...')
+
     path = os.getcwd() + '/../'
-    model_path = os.getcwd() + '/../yolov5s.pt'
+    model_path = os.getcwd() + '/yolov5s.pt'  # os.getcwd() + '/../yolov5s.pt'
+
     model = torch.hub.load(path, 'custom', path=model_path, source="local")
 
-    if not isinstance(model, models.common.AutoShape):
-        warnings.warn(f'nodel type error {type(model)}')
-        return
-
-        # or file, Path, PIL, OpenCV, numpy, list
     imgs = [
-        os.getcwd() + '/../data/images/dog.jpg',
-        os.getcwd() + '/../data/images/bus.jpg'
+        # r'E:\code\opencv_demo\yolov5\data\images\bus.jpg',
+        # r'E:\code\opencv_demo\yolov5\data\images\zidane.jpg',
+        r'E:\code\opencv_demo\demo\res\a.png',
+        # r'E:\code\opencv_demo\demo\res\b.png',
     ]
 
     print('---------------------start ...')
 
-    for img in imgs:
-        # Inference
-        results = model(img)
-        if not isinstance(results, models.common.Detections):
-            warnings.warn(f'nodel type error {type(results)}')
-            continue
+    # cap = cv2.VideoCapture(r'F:\Develop\ServerE\bvcr\src\cr\clay\demo\Camera Road 01.avi')  # but your video here
 
-        print(f'\n---------------------------- {img}')
+    index = 0
+    for img_path in imgs:
+        # Inference
+        results = model(img_path)
+        print(f'\n---------------------------- {img_path}')
 
         # Results
         results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
         result_object = results.crop(save=False)
+
+        img = cv2.imread(img_path)
 
         for i, obj in enumerate(result_object):
             idx = int(obj['cls'])
@@ -43,5 +42,15 @@ def f2():
             x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
             print(f'{i + 1}. 名称: {label:10} 可信度: {conf:.2f} 位置: ({x1:3},{y1:3}) ({x2:3},{y2:3})')
 
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.putText(img, "{}({})".format(label, int(conf * 100)), (x1 + 10, y1 + 10),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.4, (50, 50, 200), 1)
 
-f2()
+        index = index + 1
+        cv2.imshow("img {}".format(index), img)
+
+    cv2.waitKey()
+
+
+print(torch.cuda.is_available())
+hell1()
---
+++ examples/hello2.py
@@ -0,0 +1,57 @@
+import torch
+import os
+import cv2
+
+
+def hello2():
+    print('---------------------load model...')
+
+    path = os.getcwd()
+    model_path = os.getcwd() + '/yolov5s.pt'  # os.getcwd() + '/../yolov5s.pt'
+
+    # model = torch.hub.load(path, 'custom', path=model_path, source="local")  # 需要 文件: ultralytics/yolov5/hubconf.py
+    # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+    model = torch.hub.load('ultralytics/yolov5:master', 'custom', path=model_path)
+
+    imgs = [
+        # r'E:\code\opencv_demo\yolov5\data\images\bus.jpg',
+        # r'E:\code\opencv_demo\yolov5\data\images\zidane.jpg',
+        r'E:\code\opencv_demo\demo\res\a.png',
+        # r'E:\code\opencv_demo\demo\res\b.png',
+    ]
+
+    print('---------------------start ...')
+
+    # cap = cv2.VideoCapture(r'F:\Develop\ServerE\bvcr\src\cr\clay\demo\Camera Road 01.avi')  # but your video here
+
+    index = 0
+    for img_path in imgs:
+        # Inference
+        results = model(img_path)
+        print(f'\n---------------------------- {img_path}')
+
+        # Results
+        results.print()  # or .show(), .save(), .crop(), .pandas(), etc.
+        result_object = results.crop(save=False)
+
+        img = cv2.imread(img_path)
+
+        for i, obj in enumerate(result_object):
+            idx = int(obj['cls'])
+            label = results.names[idx]
+            conf = float(obj['conf'])
+            box = obj['box']
+            x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+            print(f'{i + 1}. 名称: {label:10} 可信度: {conf:.2f} 位置: ({x1:3},{y1:3}) ({x2:3},{y2:3})')
+
+            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.putText(img, "{}({})".format(label, int(conf * 100)), (x1 + 10, y1 + 10),
+                        cv2.FONT_HERSHEY_TRIPLEX, 0.4, (50, 50, 200), 1)
+
+        index = index + 1
+        cv2.imshow("img {}".format(index), img)
+
+    cv2.waitKey()
+
+
+hello2()
